c48d4128039b2d9f30faff4f2d8223f092a307bc
Eduardo Luis Borba <eduardoborba3@gmail.com>
2022-04-13 09:12:16 -0300

    Atualização swiper slider

--- src/js/swiper-slide.js
+++ src/js/swiper-slide.js
@@ -1,16 +1,13 @@
-var swiper = new Swiper('.swiper-container', {
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
   spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
+  loop: false,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
